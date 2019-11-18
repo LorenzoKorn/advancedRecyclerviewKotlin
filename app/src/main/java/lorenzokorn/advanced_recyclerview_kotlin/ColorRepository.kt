dc5b6187c924eb8725e7557fc6c48f479e78b7b4
lorenzo korn <lkorn.9520@gmail.com>
2019-11-18 11:28:59 +0100

    added extra color to the array

--- app/src/main/java/lorenzokorn/advanced_recyclerview_kotlin/ColorRepository.kt
+++ app/src/main/java/lorenzokorn/advanced_recyclerview_kotlin/ColorRepository.kt
@@ -5,6 +5,7 @@ import lorenzokorn.advanced_recyclerview_kotlin.model.ColorItem
 class ColorRepository {
     fun getColorItems(): List<ColorItem> {
         return arrayListOf(
+            ColorItem("FF00DD", "Pink"),
             ColorItem("000000", "Black"),
             ColorItem("FF0000", "Red"),
             ColorItem("0000FF", "Blue"),
